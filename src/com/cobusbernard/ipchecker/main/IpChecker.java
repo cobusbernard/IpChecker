a909051f0ae456df43863924b95f71185e6687ea
Cobus Bernard <cobus.bernard@gmail.com>
2012-02-14 12:22:53 +0200

    Changing the app to take input parameters instead of asking for input so that it can be used as a commandline tool.

--- src/com/cobusbernard/ipchecker/main/IpChecker.java
+++ src/com/cobusbernard/ipchecker/main/IpChecker.java
@@ -3,10 +3,6 @@
  */
 package com.cobusbernard.ipchecker.main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import com.cobusbernard.ipchecker.exception.InvalidCidrFormatException;
 import com.cobusbernard.ipchecker.exception.InvalidIpFormatException;
 import com.cobusbernard.ipchecker.ipaddress.CIDRBlock;
@@ -24,38 +20,21 @@ public class IpChecker {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		CIDRBlock block = null;
-		
-		while (block == null)
-		{
-			System.out.print("Please enter the CIDR block: ");
-			try {
-				block = new CIDRBlock(br.readLine());
-			} catch (IOException ioe) {
-				System.out.println("IO error trying to read CIDR block!");
-			} catch (InvalidCidrFormatException e) {
-				System.out.println(e.getMessage());
-			}
+		try {
+			
+			CIDRBlock block = new CIDRBlock(args[0]);
+			IpAddress ip = new IpAddress(args[1]);
+			
+			System.out.println("\nThe ip " + ip.toString() + " is " + (block.containsIP(ip) ? "" : "not ")
+					+ "contained in the CIDR block between " + block.getLowerIp().toString() 
+					+ " and "+ block.getUpperIp().toString() + ".");
+			
+		} catch (InvalidCidrFormatException e) {
+			System.out.println(e.getMessage());
+		} catch (InvalidIpFormatException e) {
+			System.out.println(e.getMessage());
 		}
 		
-		IpAddress ip = null;
-		
-		while (ip == null)
-		{
-			System.out.print("Please enter the IP address: ");
-			try {
-				ip = new IpAddress(br.readLine());
-			} catch (IOException ioe) {
-				System.out.println("IO error trying to read CIDR block!");
-			} catch (InvalidIpFormatException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-		
-		System.out.println("\nThe ip " + ip.toString() + " is " + (block.containsIP(ip) ? "" : "not ")
-				+ "contained in the CIDR block between " + block.getLowerIp().toString() 
-				+ " and "+ block.getUpperIp().toString() + ".");
 		
 	}
 }
